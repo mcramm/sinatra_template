09c1ca43d73580b5cdc87b0e8fa27d9638200443
mcramm <GMCramm@gmail.com>
2010-10-25 12:30:58 -0600

    Replacing all spaces/tabs in code to 4 spaces.
    Needed since I am now using vi(m) for everything

--- app.rb
+++ app.rb
@@ -9,16 +9,16 @@ set :views, "views"
 set :public, "static"
 
 configure do 
-  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
+    Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
 end
 
 get '/stylesheets/:name.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
+    content_type 'text/css', :charset => 'utf-8'
+    sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
 end
 
 get '/' do
-  haml :index
+    haml :index
 end
 
 private
